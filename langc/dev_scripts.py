9b961699f68ea28c1be7738d77bc0e30ef0cbbf8
Erick Friis <erick@langchain.dev>
2023-10-22 15:39:10 -0700

    wip

--- langc/dev_scripts.py
+++ langc/dev_scripts.py
@@ -7,21 +7,11 @@ from langserve.packages import add_package_route
 from langc.utils.packages import get_package_root
 
 
-def start_demo_server(*, port: int = 8000, host: str = "0.0.0.0"):
+def create_demo_server():
     """
-    Starts a demo server for the current hub package.
+    Creates a demo server for the current hub package.
     """
     app = FastAPI()
     package_root = get_package_root()
     add_package_route(app, package_root, "")
-    import uvicorn
-
-    # cast port as an int in case it comes as a string argument
-    # from poe
-    uvicorn.run(
-        app,
-        host=host,
-        port=int(port),
-        reload=True,
-        reload_includes=str(package_root.resolve()) + "/**/*.py",
-    )
+    return app
